16b69e3e7253437e6097eab856abd0953bd93f6b
ContrerasFernando <contreras.fernando@inventec.com>
2024-09-23 11:23:25 -0600

    Initital commit

---
+++ Controllers/WhatsAppService.cs
@@ -0,0 +1,33 @@
+﻿using Twilio;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace FISBOTTEST.Controllers
+{
+    public class WhatsAppService
+    {
+
+        private readonly string _accountSid;
+        private readonly string _authToken;
+        private readonly string _fromWhatsAppNumber;
+
+        public WhatsAppService(IConfiguration configuration)
+        {
+            _accountSid = configuration["Twilio:AccountSid"];
+            _authToken = configuration["Twilio:AuthToken"];
+            _fromWhatsAppNumber = configuration["Twilio:WhatsAppFromNumber"];
+            TwilioClient.Init(_accountSid, _authToken);
+        }
+
+        public void SendWhatsAppMessage(string to, string message)
+        {
+            var toWhatsAppNumber = $"whatsapp:{to}";
+            MessageResource.Create(
+                body: message,
+                from: new Twilio.Types.PhoneNumber(_fromWhatsAppNumber),
+                to: new Twilio.Types.PhoneNumber(toWhatsAppNumber)
+            );
+        }
+
+    }
+}
